ce8b3cbfaad13ce2c2424f4712fa00897f429c57
Doãn Huy <dzon@nuc.nuc>
2022-02-18 16:37:51 +0700

    poa

--- fullnode.sh
+++ fullnode.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ "$1" = "d" ]; then
-    echo stop chain
+    echo stop fullnode
     docker-compose -p tlx-fullnode -f docker-compose-fullnode.yml down
     exit 0
 fi
 
-echo start chain
+echo start fullnode
 docker-compose -p tlx-fullnode -f docker-compose-fullnode.yml up -d
 
